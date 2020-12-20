34a0ca236484cf76bdcc38e1defd243771583cc4
David <david.vera@9online.fr>
2020-12-20 21:46:03 +0100

    feat(react): implementing scss

--- booking-react/src/index.js
+++ booking-react/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from '../node_modules/react-dom';
 
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
-import './index.css';
+import './index.scss';
 
 import App from './components/app-component/App'
 import reportWebVitals from './reportWebVitals';
