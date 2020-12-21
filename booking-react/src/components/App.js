979bc313c9db06b9d6c7fb5e103d3e8963e8af6e
David <david.vera@9online.fr>
2020-12-21 20:38:42 +0100

    feat(react): basic routing

--- booking-react/src/components/App.js
+++ booking-react/src/components/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Header from "./shared/header-component/Header";
 import RentalHome from "./pages/home-component/RentalHome";
+import Register from "./pages/auth-component/Register";
+import Login from "./pages/auth-component/Login";
 
 const App = (props) => {
+    const renderPages = () => {
+        const {pathname} = window.location
+        switch(pathname) {
+            case '/':
+                return <RentalHome />
+            case '/login':
+                return <Login />
+            case '/register':
+                return <Register />
+            default:
+                return <RentalHome />
+        }
+    };
     return (
         <div>
             <Header />
-            <RentalHome />
+            { renderPages() }
         </div>
     )
 }
---
+++ booking-react/src/components/pages/auth-component/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Register from "./Register";
+
+class Login extends React.Component {
+    render() {
+        return(
+            <h1>Login page</h1>
+        )
+    }
+}
+
+export default Login;
