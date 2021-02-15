3f652d36cfc6213a63019f6b7612ffcb8547494d
David <david.vera@9online.fr>
2021-02-15 20:02:28 +0100

    front(lrearning): fix lint

--- booking-angular/src/app/decorators/index.ts
+++ booking-angular/src/app/decorators/index.ts
@@ -1,6 +1,8 @@
+// tslint:disable-next-line:typedef
 export function AppDecorator(config: any) {
   console.log(config.message);
 
+  // tslint:disable-next-line:only-arrow-functions
   return function(target: any) {
     console.log('Decorated class - ', target);
 
