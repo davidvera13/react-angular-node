18765c748d27db66347b0ef58e1b740c841e5898
David <david.vera@9online.fr>
2021-02-16 08:18:46 +0100

    front(learning): add bootstrap and enhance header

--- booking-angular/src/app/components/header/header.component.ts
+++ booking-angular/src/app/components/header/header.component.ts
@@ -6,9 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  welcomeHeder = 'Header component called';
-  randNumber = Math.random();
-
   constructor() { }
 
   ngOnInit(): void {
