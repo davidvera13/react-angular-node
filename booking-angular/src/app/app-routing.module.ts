7bb5fb53024d925bac089534c911eb4bc7979a1b
David <david.vera@9online.fr>
2021-04-28 23:31:45 +0200

    feat(angular): navigation, css, auth components

--- booking-angular/src/app/app-routing.module.ts
+++ booking-angular/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from './components/auth/login/login.component';
+import {RegisterComponent} from './components/auth/register/register.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/rentals', pathMatch: 'full' }
+  { path: '', redirectTo: '/rentals', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
 ];
 
 @NgModule({
--- booking-angular/src/app/app.module.ts
+++ booking-angular/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RentalModule } from './components/rentals/rental.module';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
