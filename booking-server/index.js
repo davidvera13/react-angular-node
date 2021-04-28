a6488b3640b6074caec14914c289ddfb178b4574
David <david.vera@9online.fr>
2021-04-29 00:46:24 +0200

    feat(node express): setup basic server

---
+++ booking-server/index.js
@@ -0,0 +1,12 @@
+/**
+ * we implement express npm install express --save
+ */
+
+const express = require('express')
+
+const app  = express();
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+   console.log('Server is listening on port ', PORT);
+});
