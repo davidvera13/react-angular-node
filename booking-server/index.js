2ad23017592c5031b4a1dc866eec327e056c81a1
David <david.vera@9online.fr>
2021-04-29 07:55:22 +0200

    feat(node express): refactoring routes

--- booking-server/index.js
+++ booking-server/index.js
@@ -5,6 +5,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const rentalRoutes = require('./routes/rentals.routes');
 const app  = express();
 const PORT = process.env.PORT || 3001;
 
@@ -13,63 +14,4 @@ app.listen(PORT, () => {
 });
 // middleware declaration
 app.use(bodyParser.json());
-
-
-const rentals = [
-    {
-        _id: 'fdsfsdfsds',
-        city: 'New York',
-        title: 'Very nice place'
-    },
-    {
-        _id: 'fdsfsdfsfs',
-        city: 'Berlin',
-        title: 'Very nice place too'
-    },
-    {
-        _id: 'fszfsdfsds',
-        city: 'London',
-        title: 'Great place'
-    },
-]
-// requests
-app.get('/api/v1/rentals', (req, res) => {
-    return res.json(rentals)
-});
-
-app.get('/api/v1/rentals/:rentalId', (req, res) => {
-    const rental = rentals.find(rental => {
-        // const rentalId = req.params.rentalId;
-        const {rentalId} = req.params;
-        return rental._id === rentalId;
-    })
-    return res.json(rental)
-});
-
-app.post('/api/v1/rentals', (req, res) => {
-    // we expect a payload
-    const payload = req.body;
-    rentals.push(payload);
-    return res.json({ message: `rental with id ${payload._id} was added`});
-});
-
-app.delete('/api/v1/rentals/:rentalId', (req, res) => {
-    const {rentalId} = req.params;
-    const rentalIndex = rentals.findIndex(rental => rental._id === rentalId );
-    // remove one element of the array based on index position
-    rentals.splice(rentalIndex, 1);
-    return res.json({ message: `rental with id ${rentalId} was removed`});
-
-});
-
-app.patch('/api/v1/rentals/:rentalId', (req, res) => {
-    const {rentalId} = req.params;
-    const payload = req.body;
-    const rentalIndex = rentals.findIndex(rental => rental._id === rentalId );
-
-    // let's consider we just update city and title
-    rentals[rentalIndex].city = payload.city;
-    rentals[rentalIndex].title = payload.title;
-
-    return res.json({ message: `rental with id ${rentalId} was updated`});
-})
+app.use('/api/v1/rentals', rentalRoutes);
