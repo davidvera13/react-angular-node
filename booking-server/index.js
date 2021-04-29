07674a6e50a932f1749c0652a650865c737c85df
David <david.vera@9online.fr>
2021-04-29 18:06:09 +0200

    feat(node express); setup mongodb

--- booking-server/index.js
+++ booking-server/index.js
@@ -4,10 +4,23 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+require('dotenv').config()
+
 
 const rentalRoutes = require('./routes/rentals.routes');
 const app  = express();
 const PORT = process.env.PORT || 3001;
+
+mongoose.connect(process.env.MONGO_DB, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+   useFindAndModify: false,
+   useCreateIndex: true
+}, () => {
+   // callback function
+   console.log('Connected to database')
+});
 
 app.listen(PORT, () => {
    console.log('Server is listening on port ', PORT);
