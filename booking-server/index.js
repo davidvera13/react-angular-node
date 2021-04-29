c1a00128609ec9408045dafd6196dde4594b9050
David <david.vera@9online.fr>
2021-04-29 07:23:46 +0200

    feat(node express): adding post request

--- booking-server/index.js
+++ booking-server/index.js
@@ -3,10 +3,52 @@
  */
 
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app  = express();
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
    console.log('Server is listening on port ', PORT);
+});
+// middleware declaration
+app.use(bodyParser.json());
+
+
+const rentals = [
+    {
+        _id: 'fdsfsdfsds',
+        city: 'New York',
+        title: 'Very nice place'
+    },
+    {
+        _id: 'fdsfsdfsfs',
+        city: 'Berlin',
+        title: 'Very nice place too'
+    },
+    {
+        _id: 'fszfsdfsds',
+        city: 'London',
+        title: 'Great place'
+    },
+]
+// requests
+app.get('/api/v1/rentals', (req, res) => {
+    return res.json(rentals)
+});
+
+app.get('/api/v1/rentals/:rentalId', (req, res) => {
+    const rental = rentals.find(rental => {
+        // const rentalId = req.params.rentalId;
+        const {rentalId} = req.params;
+        return rental._id === rentalId;
+    })
+    return res.json(rental)
+});
+
+app.post('/api/v1/rentals', (req, res) => {
+    // we expect a payload
+    const payload = req.body;
+    rentals.push(payload);
+    return res.json({ message: `rental with id ${payload._id} was added`});
 });
