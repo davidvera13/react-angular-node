4d082eb63f765d3c2c7ad8321bd2e0dd376df023
David <david.vera@9online.fr>
2021-04-29 08:14:28 +0200

    feat(node express): adding controller and refactoring routing

---
+++ booking-server/controllers/rentals.controller.js
@@ -0,0 +1,60 @@
+
+// fake data
+const rentals = [
+    {
+        _id: 'fdsfsdfsds',
+        city: 'New York',
+        title: 'Very nice place'
+    },
+    {
+        _id: 'fdsfsdfsfs',
+        city: 'Berlin',
+        title: 'Very nice place too'
+    },
+    {
+        _id: 'fszfsdfsds',
+        city: 'London',
+        title: 'Great place'
+    },
+]
+
+exports.getRentals = (req, res) => {
+    return res.json(rentals)
+};
+
+exports.getRental = (req, res) => {
+    const rental = rentals.find(rental => {
+        // const rentalId = req.params.rentalId;
+        const {rentalId} = req.params;
+        return rental._id === rentalId;
+    })
+    return res.json(rental)
+};
+
+exports.createRental = (req, res) => {
+    // we expect a payload
+    const payload = req.body;
+    rentals.push(payload);
+    return res.json({ message: `rental with id ${payload._id} was added`});
+}
+
+exports.updateRental = (req, res) => {
+    const {rentalId} = req.params;
+    const payload = req.body;
+    const rentalIndex = rentals.findIndex(rental => rental._id === rentalId );
+
+    // let's consider we just update city and title
+    rentals[rentalIndex].city = payload.city;
+    rentals[rentalIndex].title = payload.title;
+
+    return res.json({ message: `rental with id ${rentalId} was updated`});
+};
+
+exports.deleteRental =  (req, res) => {
+    const {rentalId} = req.params;
+    const rentalIndex = rentals.findIndex(rental => rental._id === rentalId );
+    // remove one element of the array based on index position
+    rentals.splice(rentalIndex, 1);
+    return res.json({ message: `rental with id ${rentalId} was removed`});
+
+}
--- booking-server/routes/rentals.routes.js
+++ booking-server/routes/rentals.routes.js
@@ -1,67 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+const {
+    getRentals,
+    getRental,
+    createRental,
+    updateRental,
+    deleteRental
+} = require('../controllers/rentals.controller');
 
-// fake data
-const rentals = [
-    {
-        _id: 'fdsfsdfsds',
-        city: 'New York',
-        title: 'Very nice place'
-    },
-    {
-        _id: 'fdsfsdfsfs',
-        city: 'Berlin',
-        title: 'Very nice place too'
-    },
-    {
-        _id: 'fszfsdfsds',
-        city: 'London',
-        title: 'Great place'
-    },
-]
 
 // requests
-router.get('/', (req, res) => {
-    return res.json(rentals)
-});
+router.get('/', getRentals);
 
-router.get('/:rentalId', (req, res) => {
-    const rental = rentals.find(rental => {
-        // const rentalId = req.params.rentalId;
-        const {rentalId} = req.params;
-        return rental._id === rentalId;
-    })
-    return res.json(rental)
-});
+router.get('/:rentalId',getRental);
 
-router.post('/', (req, res) => {
-    // we expect a payload
-    const payload = req.body;
-    rentals.push(payload);
-    return res.json({ message: `rental with id ${payload._id} was added`});
-});
+router.post('/', createRental);
 
-router.delete('/:rentalId', (req, res) => {
-    const {rentalId} = req.params;
-    const rentalIndex = rentals.findIndex(rental => rental._id === rentalId );
-    // remove one element of the array based on index position
-    rentals.splice(rentalIndex, 1);
-    return res.json({ message: `rental with id ${rentalId} was removed`});
+router.delete('/:rentalId', deleteRental);
 
-});
-
-router.patch('/:rentalId', (req, res) => {
-    const {rentalId} = req.params;
-    const payload = req.body;
-    const rentalIndex = rentals.findIndex(rental => rental._id === rentalId );
-
-    // let's consider we just update city and title
-    rentals[rentalIndex].city = payload.city;
-    rentals[rentalIndex].title = payload.title;
-
-    return res.json({ message: `rental with id ${rentalId} was updated`});
-})
+router.patch('/:rentalId', updateRental);
 
 // export the router and make it available to index.js
 module.exports = router;
