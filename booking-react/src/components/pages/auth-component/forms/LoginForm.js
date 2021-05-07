a3ca6f6bdd96870e6602405948aa34f80fa21425
David <david.vera@9online.fr>
2021-05-07 20:17:08 +0200

    feat(react): handling error message

--- booking-react/src/components/pages/auth-component/forms/LoginForm.js
+++ booking-react/src/components/pages/auth-component/forms/LoginForm.js
@@ -5,40 +5,81 @@ import {useForm} from "react-hook-form";
 const LoginForm = ({onSubmit}) => {
     // eslint-disable-next-line
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    // const onSubmit = (data) => {
-    //     // eslint-disable-next-line
-    //     console.log(data);
-    //     console.log("email : " + watch("email"));
-    //     console.log("passwd: " + watch("password"));
-    // }
+
+    const EMAIL_PATTERN = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <div className="form-group">
                 <label htmlFor="email">Email</label>
                 <input
-                    {...register("email")}
+                    {
+                        ...register(
+                            "email",
+                            {
+                                required: true,
+                                pattern: EMAIL_PATTERN
+                            }
+                        )
+                    }
                     name="email"
                     type="email"
                     className="form-control"
                     id="email" />
-                {/* <div className="alert alert-danger">
-            <div>
-              Email is required.
             </div>
             <div>
-              Must be valid email format!
+                {
+                    errors.email &&
+                    <div className='alert alert-danger'>
+                        {
+                            errors.email.type === 'required' &&
+                            <span>Email is required</span>
+                        }
+                        {
+                            errors.email.type === 'pattern' &&
+                            <span>Must be valid email format!</span>
+                        }
+
+                    </div>
+                }
             </div>
-          </div> */}
-            </div>
+
             <div className="form-group">
                 <label htmlFor="password">Password</label>
                 <input
-                    {...register("password")}
+                    {
+                        ...register(
+                            "password",
+                            {
+                                required: true,
+                                minLength: 5,
+                                maxLength: 20
+                            }
+                        )
+                    }
                     name="password"
                     type="password"
                     className="form-control"
                     id="password" />
+            </div>
+            <div>
+                {
+                    errors.password &&
+                    <div className='alert alert-danger'>
+                        {
+                            errors.password.type === 'required' &&
+                                <span>Password is required</span>
+                        }
+                        {
+                            errors.password.type === 'minLength' &&
+                            <span>Password is too short</span>
+                        }
+                        {
+                            errors.password.type === 'maxLength' &&
+                            <span>Password is too long</span>
+                        }
+                    </div>
+                }
             </div>
 
             <button
