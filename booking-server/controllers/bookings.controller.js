f26fc6f6433f7d4ae3e4030e54ab54141f7a8148
David <david.vera@9online.fr>
2021-06-06 13:32:56 +0200

    feat(node express); create booking

--- booking-server/controllers/bookings.controller.js
+++ booking-server/controllers/bookings.controller.js
@@ -1,7 +1,34 @@
 const Booking = require('../models/booking.model')
 
-
 exports.createBooking = (req, res) => {
     const bookingData = req.body;
-    return res.json({message: 'Booking is created!'});
+    const booking = new Booking({...bookingData, user: res.locals.user});
+    // booking.user = res.locals.user;
+
+    Booking.find({ rental: booking.rental }, (error, rentalBookings) => {
+        if(error) {
+            return res.mongoError(error);
+        }
+        // is booking valid ?
+        const isValid = isBookingValid(booking)
+        if(isValid) {
+            booking.save((error, storedBooking) => {
+                if(error) {
+                    return res.mongoError(error);
+                }
+                return res.json({
+                    message: 'Booking is created!',
+                    startAt: storedBooking.startAt,
+                    endAt: storedBooking.endAt
+                });
+            })
+        } else {
+            return res.json({message: 'Booking is not created!'});
+        }
+    });
 };
+
+// TODO: provide implementation
+function isBookingValid(booking) {
+    return true;
+}
