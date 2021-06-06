88e384d5c51c73716112e3114a0d1d80331daf04
David <david.vera@9online.fr>
2021-06-06 17:08:06 +0200

    feat(node - express): Check booking validity

--- booking-server/controllers/bookings.controller.js
+++ booking-server/controllers/bookings.controller.js
@@ -1,16 +1,24 @@
 const Booking = require('../models/booking.model')
+const moment = require('moment');
 
 exports.createBooking = (req, res) => {
     const bookingData = req.body;
     const booking = new Booking({...bookingData, user: res.locals.user});
     // booking.user = res.locals.user;
 
+    if(!isBookingDatesValid(booking)) {
+        return res.apiError({
+            title: "Invalid booking dates",
+            details: "Start date and End dates error"
+        });
+
+    }
     Booking.find({ rental: booking.rental }, (error, rentalBookings) => {
         if(error) {
             return res.mongoError(error);
         }
         // is booking valid ?
-        const isValid = isBookingValid(booking)
+        const isValid = isBookingValid(booking, rentalBookings)
         if(isValid) {
             booking.save((error, storedBooking) => {
                 if(error) {
@@ -23,12 +31,44 @@ exports.createBooking = (req, res) => {
                 });
             })
         } else {
-            return res.json({message: 'Booking is not created!'});
+            // return res.json({message: 'Booking is not created!'});
+            return res.apiError({
+                title: "Invalid booking",
+                details: "Choosen dates are already taken"
+            });
         }
     });
 };
 
 // TODO: provide implementation
-function isBookingValid(booking) {
-    return true;
+function isBookingValid(pendingBooking, rentalBookings) {
+    let isValid = true;
+    if(rentalBookings && rentalBookings.length > 0) {
+        // foreach
+        isValid = rentalBookings.every(booking => {
+            const pendingBookingStart = moment(pendingBooking.startAt);
+            const pendingBookingEnd = moment(pendingBooking.endAt);
+
+            const bookingStart = moment(booking.startAt);
+            const bookingEnd = moment(booking.endAt);
+
+            return (
+                ((bookingStart < pendingBookingStart)  && (bookingEnd < pendingBookingStart)) ||
+                ((pendingBookingEnd < bookingEnd)  && (pendingBookingEnd < bookingStart))
+            );
+        })
+    }
+    return isValid;
+}
+
+function isBookingDatesValid(booking) {
+    let isValid = true;
+    if(!booking.startAt || !booking.endAt) {
+        isValid = false;
+    }
+    if(moment(booking.startAt).isAfter(moment(booking.endAt))) {
+        isValid = false;
+    }
+
+    return isValid
 }
