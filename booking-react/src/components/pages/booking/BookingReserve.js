10a3ac20db99ebf195f1af0c994b1eb760b6355c
David <david.vera@9online.fr>
2021-06-15 18:10:36 +0200

    feat(react): modal implementation for confirmation

--- booking-react/src/components/pages/booking/BookingReserve.js
+++ booking-react/src/components/pages/booking/BookingReserve.js
@@ -1,12 +1,15 @@
 import React from "react";
 import DateRangePicker from "react-bootstrap-daterangepicker";
 import moment from "moment";
+import BookingModal from "../../shared/model-component/BookingModal";
+
+// //    "react-bootstrap-daterangepicker": "^7.0.0",
 
 class BookingReserve extends React.Component{
     constructor() {
-        super();
+        super(undefined);
 
-        // this.dateRef = React.createRef();
+        this.dateRef = React.createRef();
         this.state = {
             proposedBooking: {
                 guests: '',
@@ -28,7 +31,9 @@ class BookingReserve extends React.Component{
     // (event, picker)
     handleApply = (_, {startDate, endDate}) => {
         // console.log(moment(startDate).format('YYYY/MM/DD') + ' to ' + moment(endDate).format('YYYY/MM/DD'));
-        // this.dateRef.current.value = moment(startDate).format('YYYY/MM/DD') + ' to ' + moment(endDate).format('YYYY/MM/DD');
+        debugger;
+        this.dateRef.current.value = moment(startDate).format('YYYY/MM/DD') + ' to ' + moment(endDate).format('YYYY/MM/DD');
+        console.log(this.dateRef.current.value)
 
         this.setState({
             proposedBooking: {
@@ -37,18 +42,24 @@ class BookingReserve extends React.Component{
                 endAt: endDate
             }
         })
-        debugger;
     }
     checkInvalidDates = (date) => {
         // if date is invalid return true
         return date < moment().add(-1, 'days');
     }
     makeBooking = () => {
-
+        debugger;
         alert(JSON.stringify(this.state))
     }
+
+    formattedDate = () => {
+        console.log(this.dateRef.current ? this.dateRef.current.value : '')
+        return this.dateRef.current ? this.dateRef.current.value : '';
+    }
+
     render() {
         const { rental } = this.props;
+        const { proposedBooking } = this.state;
         return (
             <div className='booking'>
                 <h3 className='booking-price'>
@@ -61,36 +72,53 @@ class BookingReserve extends React.Component{
                 <div className='form-group'>
                     <label htmlFor='dates'>Dates</label>
                     <DateRangePicker
-                        initialSettings={{
-                            opens:"left",
-                            containerStyles:{ display: 'block'},
-                            isInvalidDate: this.checkInvalidDates
-
-                        }}
                         onApply={this.handleApply}
-                    >
+                        opens="left"
+                        containerStyles={{display: 'block'}}
+                        isInvalidDate={this.checkInvalidDates}>
                         <input
-                            // ref={this.dateRef}
+                            ref={this.dateRef}
                             id="dates"
-                            type='text'
-                            className='form-control'
-                        />
+                            type="text"
+                            className="form-control">
+                        </input>
                     </DateRangePicker>
                 </div>
                 <div className='form-group'>
                     <label htmlFor='guests'>Guests</label>
                     <input
                         onChange={this.handleGuestsChange}
-                        value={this.state.proposedBooking.guests}
+                        value={proposedBooking.guests}
                         type='number'
                         className='form-control'
                         id='guests'
                         aria-describedby='guests'>
                     </input>
                 </div>
-                <button
-                    onClick={this.makeBooking}
-                    className='btn btn-bwm-main btn-block'>Reserve place now</button>
+                {/*<button*/}
+                {/*    onClick={this.makeBooking}*/}
+                {/*    className='btn btn-bwm-main btn-block'>Reserve place now*/}
+                {/*</button>*/}
+
+
+                {/* passing button as a prop */}
+                <BookingModal
+                    onSubmit={this.makeBooking}
+                    title="Booking confirmation"
+                    subtitle={"Date : " + this.formattedDate() }
+                    openBtn={
+                        <button
+                            className='btn btn-bwm-main btn-block'>Reserve place now
+                        </button>
+                    }
+                >
+                    {/*<p className='modal-subtitle'>Date : {this.formattedDate()}</p>*/}
+                    <em>12 </em>Nights / <em>$ {rental.dailyPrice}</em> per night
+                    <p>Guests : { proposedBooking.guests }</p>
+                    <p>Price : <em>$ 200</em></p>
+                    <p>Do you confirm your booking ?</p>
+                </BookingModal>
+
                 <hr></hr>
                 <p className='booking-note-title'>People are interested into this house</p>
                 <p className='booking-note-text'>
---
+++ booking-react/src/components/shared/model-component/BookingModal.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import Modal from 'react-responsive-modal';
+import 'react-responsive-modal/styles.css'
+
+// we pass openBtn from parent component to the child and we define an alias
+const BookingModal = ({
+    title = "Default Modal",
+    subtitle = "confirm Data",
+    children,
+    onSubmit,
+    openBtn: OpenBtn
+}) => {
+    const [isOpen, setIsOpen ] = useState(false);
+    return (
+        <>
+            {!OpenBtn &&
+                <button
+                    className="btn btn-success"
+                    onClick={() => setIsOpen(true)}>
+                    Open
+                </button>
+            }
+            {OpenBtn &&
+                <div onClick={() => setIsOpen(true)}>
+                    {OpenBtn}
+                </div>
+            }
+
+            <Modal
+                focusTrapped={false}
+                open={isOpen}
+                onClose={() => setIsOpen(false) }
+                classNames={{ modal: 'booking-modal' }}
+            >
+                <h4 className='modal-title title'>{ title }</h4>
+                <p className='modal-subtitle'>Subtitle : { subtitle }</p>
+                <div className='modal-body'>
+                    { children }
+                </div>
+                <div className='modal-footer'>
+                    <button
+                        type='button'
+                        className='btn btn-booking-main '
+                        onClick={onSubmit}
+                    >
+                        Confirm
+                    </button>
+                    <button
+                        type='button'
+                        className='btn btn-alert'
+                        onClick={() => setIsOpen(false)}
+                    >
+                        Cancel
+                    </button>
+                </div>
+            </Modal>
+        </>
+    );
+}
+export default BookingModal;
